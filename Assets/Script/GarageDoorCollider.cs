a2236b1c0b5a6ef2b7fcb4d7a1f167cb24725ec8
Josh Gifford <Joshuagifford@gmail.com>
2018-09-25 22:46:02 -0600

    Garage Door Prefab

---
+++ Assets/Script/GarageDoorCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarageDoorCollider : MonoBehaviour
+{
+
+    GameObject garageDoor;
+    GameObject garageDoormat;
+
+    void Start()
+    {
+        garageDoor = gameObject;
+        garageDoormat = garageDoor.transform.Find("Garage Door").gameObject;
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject.Find("Garage Door").SetActive(false); //this makes the door disappear when the player triggers it
+        GameObject.Find("Garage Brick Walls").SetActive(false);
+        GameObject.Find("Garage Roof").SetActive(false);
+
+        garageDoormat.SetActive(true);
+        garageDoor.SetActive(false);
+        
+    }
+
+}
---
+++ Assets/Script/GarageDoormatCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarageDoormatCollider : MonoBehaviour {
+
+
+    GameObject GarageDoor;
+    GameObject GarageDoormat;
+    GameObject GarageBrickWall;
+    GameObject GarageRoof;
+
+    void Start()
+    {
+
+        GarageDoor = GameObject.Find("Garage Door");
+        GarageDoormat = GameObject.Find("Garage Doormat");
+        GarageBrickWall = GameObject.Find("Garage Brick Walls");
+        GarageRoof = GameObject.Find("Garage Roof");
+
+        GarageDoormat.SetActive(false);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        GarageDoor.SetActive(true);
+        GarageDoormat.SetActive(false);
+        GarageBrickWall.SetActive(true);
+        GarageRoof.SetActive(true);
+    }
+}
